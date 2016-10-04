86c3b8dc2336fd5e85cdeebf02518855df7d30c7
duongmd <manhduong1986@gmail.com>
2016-10-04 10:47:57 +0700

    Để 2 button về 1 bên trên navigation bar

--- NavController-TabbarController/NavController-TabbarController/YourBookController.swift
+++ NavController-TabbarController/NavController-TabbarController/YourBookController.swift
@@ -12,11 +12,34 @@ class YourBookController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
-
+        
+        //Chọn button theo kiểu có sẵn
+        let cameraButtonItem = UIBarButtonItem(barButtonSystemItem: .Camera, target: self, action: #selector(takeAPicture(_:)))
+        //navigationItem.leftBarButtonItem = cameraButtonItem
+        
+        //Chọn button theo ảnh trong Assets
+        let rightButton = UIBarButtonItem(image: UIImage(named: "EMAIL"), style: .Plain, target: self, action: #selector(senEmail(_:)))
+        
+        var fixedSpace = UIBarButtonItem(barButtonSystemItem: .FixedSpace, target: nil, action: nil)
+        fixedSpace.width = 0.0
+        
+        
+        
+        navigationItem.setRightBarButtonItems([rightButton,fixedSpace,cameraButtonItem], animated: true)
     }
-
+    
+    func takeAPicture(sender: AnyObject) {
+        print("Hello")
+    }
+    
+    func senEmail(sender: AnyObject) {
+        print("Sending Email...")
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
